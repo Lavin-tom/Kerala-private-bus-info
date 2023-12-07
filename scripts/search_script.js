f80e9d5cef61f464db885e333e0ca751bf372eb7
Lavin Tom K Abraham <53207510+Lavin-tom@users.noreply.github.com>
2023-12-07 21:30:30 +0530

    Update search_script.js

--- scripts/search_script.js
+++ scripts/search_script.js
@@ -96,74 +96,69 @@ var busStations = {
 				throw error;	
 			}	
     }
-function search() {
+async function search() {
     var dropdown1Value = document.getElementById("dropdown1").value;
     var dropdown2Value = document.getElementById("dropdown2").value;
     var dropdown3Value = document.getElementById("dropdown3").value;
 
     console.log("Searching with values:", dropdown1Value, dropdown2Value, dropdown3Value);
 
-
     var jsonFilePath = 'res/' + dropdown1Value.toLowerCase() + '.json';
 
+    try {
+        const response = await fetch(jsonFilePath);
+        const jsonData = await response.json();
 
-    fetchJsonData(jsonFilePath)
-        .then(data => {
-            const allTrips = [];
+        const allTrips = [];
 
+        if (jsonData.busSchedules) {
+            jsonData.busSchedules.forEach(schedule => {
+                const routeIndex = schedule.route.indexOf(dropdown2Value);
+                const destinationIndex = schedule.route.indexOf(dropdown3Value);
 
-            if (data.schedule) {
+                if (routeIndex >= 0 && destinationIndex >= 0 && routeIndex < destinationIndex) {
+                    const selectedStations = schedule.route.slice(routeIndex, destinationIndex + 1);
 
-                Object.keys(data.schedule).forEach(tripNumber => {
-                    const tripSchedule = data.schedule[tripNumber];
-                    const stations = tripSchedule.stations;
+                    const tripData = schedule.schedule.map(trip => {
+                        const stations = trip.stations.filter(station => selectedStations.includes(station.station.trim()));
+                        return {
+                            vehicleNumber: schedule["Vehicle Number"],
+                            tripNumber: trip.trip,
+                            departureStation: stations[0].station.trim(),
+                            arrivalStation: stations[stations.length - 1].station.trim(),
+                            departureTime: stations[0].departureTime,
+                        };
+                    });
 
-                    const routeIndex = stations.findIndex(station => station.station.trim() === dropdown2Value.trim());
-                    const destinationIndex = stations.findIndex(station => station.station.trim() === dropdown3Value.trim());
-
-                    if (routeIndex >= 0 && destinationIndex >= 0) {
-                        const selectedStations = routeIndex < destinationIndex
-                            ? stations.slice(routeIndex, destinationIndex + 1)
-                            : stations.slice(destinationIndex, routeIndex + 1).reverse();
-
-                        if (selectedStations.length === Math.abs(routeIndex - destinationIndex) + 1 &&
-                            selectedStations[0].station.trim() === dropdown2Value.trim() &&
-                            selectedStations[selectedStations.length - 1].station.trim() === dropdown3Value.trim()) {
-                            allTrips.push({
-                                vehicleNumber: data["Vehicle Number"],
-                                tripNumber: tripNumber,
-                                departureStation: selectedStations[0].station.trim(),
-                                arrivalStation: selectedStations[selectedStations.length - 1].station.trim(),
-                                departureTime: selectedStations[0].departureTime,
-                            });
-                        }
-                    }
-                });
-            } else {
-                console.error('Invalid data structure. Expected "schedule" property to exist.');
-            }
-
-
-            allTrips.sort((a, b) => {
-                const timeA = new Date(a.departureTime).getTime();
-                const timeB = new Date(b.departureTime).getTime();
-                return timeA - timeB;
+                    allTrips.push(...tripData);
+                }
             });
+        } else {
+            console.error('Invalid data structure. Expected "busSchedules" property to exist.');
+        }
 
-            displayResults(allTrips);
+        allTrips.sort((a, b) => {
+            const timeA = new Date(a.departureTime).getTime();
+            const timeB = new Date(b.departureTime).getTime();
+            return timeA - timeB;
+        });
 
-            const resultTable = document.getElementById('resultTable');
-            const noRouteMessage = document.getElementById('noRouteMessage');
-            if (allTrips.length > 0) {
-                resultTable.style.display = 'table';
-                noRouteMessage.style.display = 'none';
-            } else {
-                resultTable.style.display = 'none';
-                noRouteMessage.style.display = 'block';
-            }
-        })
-        .catch(error => console.error('Error fetching JSON data:', error));
+        displayResults(allTrips);
+
+        const resultTable = document.getElementById('resultTable');
+        const noRouteMessage = document.getElementById('noRouteMessage');
+        if (allTrips.length > 0) {
+            resultTable.style.display = 'table';
+            noRouteMessage.style.display = 'none';
+        } else {
+            resultTable.style.display = 'none';
+            noRouteMessage.style.display = 'block';
+        }
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
 }
+
 function displayResults(results) {
     const tableBody = document.getElementById('resultTable').getElementsByTagName('tbody')[0];
 
