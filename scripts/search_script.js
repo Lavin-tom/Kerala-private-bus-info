8b3dd93e11b0d5ce8f38d2b9958225997116bfc5
Lavin Tom K Abraham <53207510+Lavin-tom@users.noreply.github.com>
2023-12-09 11:13:38 +0530

    Update search_script.js

--- scripts/search_script.js
+++ scripts/search_script.js
@@ -1,102 +1,3 @@
-var options1 = ["Choose District","THIRUVANANTHAPURAM","KOLLAM","PATHANAMTHITTA","ALAPUZHA","KOTTAYAM","IDUKKI","ERNAKULAM","TRISSUR","PALAKKAD","MALAPURAM","KOZHIKODE","WAYANAD","KANNUR","KASARAGOD"];	
-var option2 = {};	
-var busStations = {	
-    "KOTTAYAM": ['116 COLONY ', '14TH MILE ', '504 COLONY ', 'ADICHIPUZHA ', 'ADIVARAM ', 'ADUKKAM ', 'ALUMTHARA ',	
-				'AMBALAKAVALA ', 'AMBAYATHODE ', 'AMBEDKAR COLONY ', 'AMBIKA MARKET ', 'ANAKAYAM ', 'ANAKKALLU ',	
-				'ANAPPARA ', 'ANCHUKUDIYAR ', 'ANCHUKUDIYAR, BHARANANGANAM ', 'ANGELVALLEY ', 'ANICHUVADU ',	
-				'ANJILITHANAM ', 'ANTHIYALAM ', 'ARUNAPURAM ', 'ATHIKKAYAM ', 'ATTIPEEDIKA ', 'ATTUVELIKADAVU ',	
-				'AYAMKUDY ', 'AYARKUNNAM ', 'AYMANAM ', 'BELIEVERS CHURCH ', 'BHARANANGANAM ', 'BLOCK ', 'BOLGATTY FERRY ',	
-				'BOYCEE ', 'BRAHMAMANGALAM ', 'BUND ROAD ', 'CHAMAKKALA ', 'CHAMAMPATHAL ', 'CHAMPAKARA TEMPLE ',	
-				'CHANGANACHERY JETTY ', 'CHANGANASSERY ', 'CHANGANASSERY NEW BUS STAND ', 'CHANGANASSERY OLD BUSTAND ',	
-				'CHANGANASSERY, PERUNNA ', 'CHANGANASSERY,(PERUNNA) ', 'CHANJODY ', 'CHANNANIKKAD ', 'CHATHANTHARA ',	
-				'CHEEPUNKAL ', 'CHEERANCHIRA ', 'CHEMMANAKARY ', 'CHENAPPADY ', 'CHENGAKKUNNU ', 'CHENGALAM JN ',	
-				'CHENGALATHU KAVU ', 'CHENGANNUR ', 'CHENNADU ', 'CHERPUNKAL ', 'CHERTHALA ', 'CHERUPUSHPAM PALLY ',	
-				'CHETHIMUKKU ', 'CHETTIMUKKU JN ', 'CHETTUTHODU ', 'CHINGAVANAM ', 'CHIRAKKADAVU ', 'CHITTAR ',	
-				'CHOLATHADAM ', 'CHOTTANIKKARA ', 'CHOVOOR ', 'CHUNKAPPARA ', 'COLONY ', 'EDAKADATHY ', 'EDAKKUNNAM ',	
-				'EDAMATTOM ', 'EDAMURI ', 'EDAYAZHAM ', 'ELAMKAD TOP ', 'ELAMKADU ', 'ELAMPALLY ', 'ELANJI ',	
-				'ELIVALIKKARA ', 'ERATTAYAR JN ', 'ERATTUPETTA ', 'ERATTUPETTA PVT BUS STAND ', 'ERNAKULAM ',	
-				'ERNAKULAM KALOOR BUS STAND ', 'ERUMELI ', 'ERUMELY ', 'ESI VADAVATHOOR ', 'ETTUMANOOR ', 'EZHACHERRY ',	
-				'EZHUMANTHURUTHU ', 'GURUVAYUR ', 'INCHAKUZHY ', 'IRITTY ', 'IRUMPOONNIKARA ', 'ITTIAPPARA ',	
-				'KADAPLAMATTOM ', 'KADAPPOOR ', 'KADATHURUTHY ', 'KADUMOONCHIRA ', 'KADUTHURUTHY ', 'KADUTHURUTHY MARKET ',	
-				'KAINADY ', 'KAIPPUZHAMUTTU ', 'KAIPUZHA ', 'KAIPUZHA SASTHANGAL ', 'KAIPUZHAMUTTU ', 'KAKKANAD ', 'KAKKOMBU ',	
-				'KALAKOODU ', 'KALATHIPADY ', 'KALAYAKANDAM ', 'KALLAM ', 'KALLAMPARA ', 'KALLARA ', 'KALLINGATHARA ',	
-				'KALLUMKATHARA ', 'KALLUNGATHARA CHURCH ', 'KALLUPURACKAL ', 'KANAMALA ', 'KANDAMCHIRA JN ', 'KANDANKAVU ',	
-				'KANGAZHA ', 'KANGAZHA HOSPITAL ', 'KANJIRAM JETTY ROAD ', 'KANJIRAMATTOM ', 'KANJIRAPPALLY ',	
-				'KANJIRAPPALLY BUS STAND ', 'KANJIRAPPALLY PVT BUS STAND ', 'KAPPADU ', 'KARIMANNOOR ', 'KARIPPOOTHATTU ',	
-				'KARIPPUTHATTU ', 'KARIPPUTHATTU BRIDGE ', 'KARIPUTHATTU ', 'KARUKACHAL ', 'KATTAPPANA ',	
-				'KATTAPPANA PVT BUS STAND ', 'KAVANALKADAVU ', 'KAVANATTINKARA ', 'KAVUMKANDOM ',	
-				'KAYAMKULAM PRIVATE BUS STAND ', 'KAYYUR ', 'KEZHUVANKULAM ', 'KIDANGOOR ',	'KIDANGOOR TEMPLE ', 'KISSUMUM ',	
-				'KOCHMATTOM ', 'KOCHUROAD ', 'KODIKUTHY ', 'KODUNGOOR ', 'KOKKAYAR VEMBLY ', 'KOLENCHERY ', 'KOLENCHERY M M HOSPITAL ',	
-				'KOLLADU ', 'KOLLADU BOATJETTY JN ', 'KOMBUKUTHY ', 'KONCHUMADA ', 'KOORAMTHOOKKU ', 'KOOROPPADA ', 'KOOTHATTUKULAM ',	
-				'KOOTTICKAL ', 'KOOVAPPALLY ', 'KORUTHODU ', 'KOSAMATTOMPADY ', 'KOTTARAMATTOM ', 'KOTTARAMATTOM BUS STAND ', 'KOTTARAMKADA ',	
-				'KOTTAYAM ', 'KOTTAYAM BUS STAND ', 'KOZHENCHERRY ', 'KOZHENCHERY ', 'KOZHIKODE ', 'KULATHOORMOOZHY ', 'KUMARAKOM ',	
-				'KUMARAKOM BOAT JETTY ', 'KUMARAKOM MARKET JUNCTION ', 'KUMILY JN ', 'KUNINJI ', 'KUNNAMTHANAM ', 'KUNNONNI ', 'KUNNUMPURAM ',	
-				'KURAVILANGAD ', 'KURICHY ', 'KURUMBANMOOZHY ', 'KURUMPANMOOZHY ', 'KURUMULLOOR CHURCH ', 'KURUPPANTHARA ', 'KURUPPUMTHARA ',	
-				'KUTTISSERIKADAVU ', 'KUZHIMAVU ', 'LAKKATTOOR ', 'MADATHILKAVALA ', 'MALAYALAPPUZHA ', 'MALIKA KADAVU ', 'MALIKAPPADY ',	
-				'MALLAPPALLY ', 'MALLAPPALLY BUS STAND ', 'MAMOODU ', 'MANACKACHIRA ', 'MANALELPALLY ', 'MANARCADU CHURCH ', 'MANARCADU JN. ',	
-				'MANCOMBU ', 'MANGALAGIR ', 'MANGAPETTA ', 'MANGARAGIRI ', 'MANIMALA ', 'MANIYAPARAMBU ', 'MANJAKUZHY ', 'MANJOOR ',	
-				'MANKOMBU TEMPLE ', 'MANNADISALA ', 'MANNAM ', 'MANNAM TEMPLE ', 'MANNANAM ', 'MARAMON JN ', 'MARANGATTUPILLY ',	
-				'MARMALA ROAD JN. ', 'MATHAMBA ', 'MATHUNKAL ', 'MATTAKARA ', 'MAVELIKARA ', 'MAVELIKKARA ', 'MCH ', 'MECHAL ',	
-				'MED. COLLEGE ', 'MEDICAL COLLEGE BUS STAND ', 'MEDICAL COLLEGE HOSPITAL ', 'MEDICAL COLLGE BUS STAND KOTTAYAM ',	
-				'MEENADAM CATTLE MARKET ', 'MELADUKKAM ', 'MELUKAVUMATTAM ', 'MEVIDA ', 'MG UNIVERSITY ', 'MONIPALLY ', 'MOOKKENPETTY ',	
-				'MOOLAMATTOM JN ', 'MOOLEDOM ', 'MOOLEPEEDIKA ', 'MOONGAMKUZHY ', 'MOOTHEDATHUKAVU ', 'MOSCOW ', 'MOSCOW JN ', 'MOTHIRAVAYAL ',	
-				'MUKKADA JN ', 'MUKKOOR ', 'MUKKOOTTUTHARA ', 'MULAYAMVELI ', 'MUNDAKAYAM ', 'MUNDAKAYAM PVT BUS STNAD ', 'MUNDIAPPALLY ',	
-				'MUNDIYAKAL ', 'MUNICIPAL BUS STAND, PALA ', 'MUTTOM BUS STAND ', 'MUTTOM JN ', 'MUVATTUPUZHA ', 'NALAKAVALA ', 'NARAKATHODE ',	
-				'NARANAMTHODU ', 'NATTAKOM GUEST HOUSE ', 'NEDUMKUNNAM ', 'NEELOOR ', 'NEENDOOR ', 'NEERIKKAD ', 'NEEZHOOR ', 'NEREKADAV ',	
-				'NEREKADAVU FERRY ', 'NJALIYAKUZH ', 'NJALIYAKUZHY ', 'NJANDUKALLU ', 'NJEEZHOOR ', 'ONAMTHUURTHU ', 'PACHIMA ', 'PADUVA ',	
-				'PAIKA ', 'PAIPPAD ', 'PAKKANAM ', 'PAKKIL ', 'PAKKILKAVALA ', 'PALA ', 'PALA KOTTARAMATTOM STAND ', 'PALA MUN STAND ',	
-				'PALA OLD BUS STAND ', 'PALA PRIVATE BUS STAND ', 'PALA TOWN STAND ', 'PALAAKKAD CHURCH ', 'PALACHUVADU ', 'PALACKATHADOM ',	
-				'PALAMATTAM ', 'PALAMKADAVU ', 'PALAPRA TEMPLE ', 'PALLICKATHODU ', 'PALLIKADAVU ', 'PAMPADY ', 'PAMPADY DAYARA ', 'PAMPAVALLY ',	
-				'PANACHIKAADU ', 'PANAMATTOM ', 'PANAMBUKADU ', 'PARAMATTOM ', 'PARATHANAM ', 'PARIPPU ', 'PARUTHUMPARA JN ', 'PATHAMPUZHA ',	
-				'PATHANADU ', 'PATHANAMTHITTA ', 'PATHINANCHIL CHIRA ', 'PATHINANCHIL KADAVU ', 'PATHINARIL CHIRA ', 'PATHINARILCHIRA ', 'PAYIPPADU JN ',	
-				'PAYYAPPADY ', 'PERINGULAM ', 'PERUMPUZHAKADAVU ', 'PERUMTHURUTHU ', 'PERUNNA ', 'PERUNNAD ', 'PERUNNADU ', 'PERUVA ',	
-				'PIRAVOM ', 'PLASANAL ', 'PODIMATTOM ', 'PONGAMTHANAM ', 'PONKUNNAM ', 'PONKUNNAM BUS STAND ', 'PONTHENPUZHA ', 'POONJAR ',	
-				'POOTHOTTA ', 'POOVANTHURUTHU ', 'POOVASSERY ', 'POOVATHILAPPU ', 'POTTUKULAM ', 'PRAVATTOM ', 'PRAVITHANAM ', 'PULIKKUNNU ',	
-				'PULIKUTTISSERY ', 'PULIKUZHY COLONY ', 'PULLARIKUNNU ', 'PUNALUR JN ', 'PUNCHAVAYAL ', 'PUNNAKKAL NIRAVU ', 'PUNNAKKUZHI ',	
-				'PUNNATHURA ', 'PUNNAVELI ', 'PUNNILAM BUS STOP ', 'PUTHENPALAM ', 'PUTHENPALLY ', 'PUTHUPALLY ', 'PUTHUPPALLY ', 'RAMAPURAM ',	
-				'RANNI ', 'RANNI ,ITTIYAPPARA ', 'S N PURAM ', 'SANTHIPURAM ', 'SASTHANADA ', 'SIVAJINAGAR ', "ST. XAVIER'S COLLEGE, KOTHAVARA, ",	
-				'SVR NSS COLLEGE ', 'T V PURAM ', 'TEEKOY ', 'THACHAPUZHA ', 'THALASSERRY ', 'THALASSERY ', 'THALAYOLAPARAMBU ', 'THAMARAKADU JN ',	
-				'THAMPALAKAD ', 'THEKKEMAAL ', 'THEKKEMALA ', 'THEMPRAVAL KADAVU ', 'THENGANA ', 'THEVALAKADU ', 'THEVARUPARA ', 'THIDANADU ', 'THIRUVALLA ',	
-				'THIRUVANCHOOR ', 'THIRUVARPPU ', 'THIRUVARPPU TEMPLE ', 'THODUPUZHA ', 'THOTTABHAGOM ', 'THOTTACKAD HOSPITAL ', 'THOTTACKADU ', 'THULAPPALLY ',	
-				'THUMARAMPARA ', 'TOLL ', 'ULLALA ', 'UMIKUPPA ', 'UMMIKUPPA ', 'UNIVERSITY ', 'UZHAVOOR ', 'VADAKKEMALA ', 'VADASSERIKARA ', 'VADAVATHOOR ',	
-				'VADAVATHOOR EAST ', 'VADAVATHOOR ESI ', 'VADAVATHOOR KADAVU ', 'VAIKKOM ', 'VAIKOM ', 'VAIPPUR ', 'VAKKAD ', 'VALLIYAMKAVU ', 'VATTAKKAVU ',	
-				'VATTUPURA ', 'VAYALA ', 'VAZHAMANA ', 'VECHOOCHIRA ', 'VECHOOR ', 'VELLANY ', 'VELLAVOOR ', 'VELLOOPARAMBU ', 'VELLOOR ', 'VELLOOR H P C ',	
-				'VELLUTHURUTHI TEMPLE ', 'VELOOR ', 'VENGOTTA ', 'VENKOTTA ', 'VETTIMUKAL ', 'VYTTILA HUB ', 'VYTTILA JN. '],	
-    "KOLLAM": ["StationX", "StationY", "StationZ"],	
-};	
-	function loadOptions(selectId, options) {	
-    var select = document.getElementById(selectId);	
-    select.innerHTML = '';	
-
-    options.forEach(function (option) {	
-        var optionElement = document.createElement("option");	
-        optionElement.value = option;	
-        optionElement.text = option;	
-        select.add(optionElement);	
-    });	
-	}	
-	loadOptions("dropdown1", options1);	
-
-	document.getElementById("filterButton").addEventListener("click", function () {	
-		var selectedValue = document.getElementById("dropdown1").value;	
-		loadDropdown2(selectedValue);	
-	});	
-	function loadDropdown2(selectedValue) {	
-		var options2Array = busStations[selectedValue] || []; 	
-		loadOptions("dropdown2", options2Array);	
-		loadOptions("dropdown3",options2Array);	
-	}	
-		async function fetchJsonData(file) {	
-			try {	
-				const response = await fetch(file);	
-				const jsonData = await response.json();	
-				return jsonData;	
-			} catch (error) {	
-				console.error('Error fetching JSON data:', error);	
-				throw error;	
-			}	
-    }
-
 async function search() {
     try {
         const dropdown1Value = document.getElementById("dropdown1").value;
@@ -125,29 +26,37 @@ async function search() {
                 const destinationIndex = schedule.route.indexOf(dropdown3Value);
 
                 if (routeIndex >= 0 && destinationIndex >= 0 && routeIndex < destinationIndex) {
-                    schedule.schedule.forEach(trip => {
-                        const stations = trip.stations;
+                    const selectedTrip = schedule.schedule.find(trip => {
                         const startIndex = schedule.route.findIndex(station => station === dropdown2Value);
                         const endIndex = schedule.route.findIndex(station => station === dropdown3Value);
+                        const stations = trip.stations;
 
-                        if (startIndex !== -1 && endIndex !== -1 && endIndex > startIndex) {
-                            const selectedStations = stations.slice(startIndex, endIndex + 1);
-
-
-                            tableHead.style.display = '';
-
-                            const row = tableBody.insertRow();
-                            const cell1 = row.insertCell(0);
-                            const cell2 = row.insertCell(1);
-                            const cell3 = row.insertCell(2);
-                            const cell4 = row.insertCell(3);
-
-                            cell1.textContent = schedule["Vehicle Number"];
-                            cell2.textContent = selectedStations[0].station.trim();
-                            cell3.textContent = selectedStations[selectedStations.length - 1].station.trim();
-                            cell4.textContent = selectedStations[0].departureTime;
-                        }
+                        return (
+                            startIndex !== -1 &&
+                            endIndex !== -1 &&
+                            endIndex > startIndex &&
+                            stations.length > endIndex &&
+                            stations[startIndex].station === dropdown2Value &&
+                            stations[endIndex].station === dropdown3Value
+                        );
                     });
+
+                    if (selectedTrip) {
+
+                        tableHead.style.display = '';
+
+                        const stations = selectedTrip.stations;
+                        const row = tableBody.insertRow();
+                        const cell1 = row.insertCell(0);
+                        const cell2 = row.insertCell(1);
+                        const cell3 = row.insertCell(2);
+                        const cell4 = row.insertCell(3);
+
+                        cell1.textContent = schedule["Vehicle Number"];
+                        cell2.textContent = stations[0].station.trim();
+                        cell3.textContent = stations[stations.length - 1].station.trim();
+                        cell4.textContent = stations[0].departureTime;
+                    }
                 }
             });
         } else {
